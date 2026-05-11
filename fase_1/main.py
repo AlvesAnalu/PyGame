6814d0dc95d869a69e5ed03d3b89b1964493c3d2
Maria Vitória <vivirametta@gmail.com>
2026-05-11 18:35:47 -0300

    implementando funcoes na classe de movimentos

--- fase_1/main.py
+++ fase_1/main.py
@@ -37,6 +37,33 @@ class AbstractCar:
     Centraliza a lógica de movimento, rotação, aceleração e colisão,
     permitindo que diferentes carros compartilhem o mesmo comportamento.
     """
+    def _init_(self, max_vel, rotation_vel):
+        """Inicializa os atributos principais do carro."""
+        self.img = self.IMG
+        self.max_vel = max_vel
+        self.vel = 0
+        self.rotation_vel = rotation_vel
+        self.angle = 0
+        self.x, self.y = self.START_POS
+        self.acceleration = 0.1
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+
+    def rotate(self, left=False, right=False):
+        """
+        Rotaciona o carro para a esquerda ou para a direita.
+
+        No contexto do autorama, isso representa a mudança de direção
+        enquanto o carrinho percorre a pista.
+        """
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+
+    def draw(self, win):
+        """Desenha o carro na tela já considerando sua rotação atual."""
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
 
 
 
