4ff4761b5cbae4d6d1b916b3a2ce8b6895f3c47d
LPR7124 <liviapr@al.insper.edu.br>
2026-05-11 18:02:53 -0300

    img carros

--- fase_1/main.py
+++ fase_1/main.py
@@ -18,3 +18,15 @@ IMG_PATH = os.path.join(FILE_PATH, "imgs")
 GRASS = scale_image(pygame.image.load(os.path.join(IMG_PATH, "grass.jpg")), 2.5)
 TRACK = scale_image(pygame.image.load(os.path.join(IMG_PATH, "track.png")), 1)
 
+# Imagem da borda da pista, usada para detectar colisões.
+# Ela deve ter o mesmo fator de escala da pista.
+TRACK_BORDER = scale_image(pygame.image.load(os.path.join(IMG_PATH, "track-border.png")), 1)
+
+RED_CAR = scale_image(pygame.image.load(os.path.join(IMG_PATH, "red-car.png")), 0.55)
+GREEN_CAR = scale_image(pygame.image.load(os.path.join(IMG_PATH, "green-car.png")), 0.55)
+
+WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Autorama 2 Jogadores")
+
+FPS = 60
